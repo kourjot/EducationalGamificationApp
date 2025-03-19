c3df86f99248081f3fe9def20cd5758583d2e171
kourjot <pkour6188@gmail.com>
2025-03-19 12:16:40 +0530

    token middleware Done

---
+++ backend/middleware/token.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+import "dotenv/config"
+const tokenverify = async(req,res,next)=>{
+    const token=req.headers["authorization"]
+    try{
+        const verify=jwt.verify(token,process.env.SECRET_KEY)
+        if(!verify){
+            return res.status(401).json({error:"Token is not valid"})
+        }
+        next()
+
+    }
+    catch(err){
+        return res.status(401).json({error:"Token is not valid"})
+    }
+}
+export {tokenverify}
