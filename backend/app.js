09abbbffdc719686571780fe00ef0b4de5f395e4
kourjot <pkour6188@gmail.com>
2025-03-19 12:31:40 +0530

    Correct Cors Error

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 import express from "express"
 import {connection} from "./config/db.js"
+import cors from "cors"
 import "dotenv/config"
 import {router} from "./routes/userRouter.js"
+
 const app=express()
+
+app.use(cors())
 app.use(express.json())
 
 app.use(router)
